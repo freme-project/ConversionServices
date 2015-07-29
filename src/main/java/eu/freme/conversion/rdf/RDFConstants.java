0b88fa4c5a54b4ac0c100d358365732920715b71
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2015-07-29 15:55:58 +0200

    Added content type to the RDFSerialization enum; closes #3

--- src/main/java/eu/freme/conversion/rdf/RDFConstants.java
+++ src/main/java/eu/freme/conversion/rdf/RDFConstants.java
@@ -10,6 +10,22 @@ public class RDFConstants {
 	public static final String xsdPrefix = "http://www.w3.org/2001/XMLSchema#";
 
 	public enum RDFSerialization {
-		TURTLE, JSON_LD, PLAINTEXT, RDF_XML, N3, N_TRIPLES, JSON
+		TURTLE("text/turtle"),
+		JSON_LD("application/json+ld"),
+		PLAINTEXT("text"),
+		RDF_XML("application/rdf+xml"),
+		N3("text/n3"),
+		N_TRIPLES("application/n-triples"),
+		JSON("application/json");
+
+		private final String contentType;
+
+		RDFSerialization(String contentType) {
+			this.contentType = contentType;
+		}
+
+		public String contentType() {
+			return contentType;
+		}
 	}
 }
