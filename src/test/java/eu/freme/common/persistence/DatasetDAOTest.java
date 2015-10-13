55e78d5305fd57d1270fb1b8f262f0b503407346
Arne Binder <arne.b.binder@gmail.com>
2015-10-13 19:52:22 +0200

    improved DatasetDAOTest

--- src/test/java/eu/freme/common/persistence/DatasetDAOTest.java
+++ src/test/java/eu/freme/common/persistence/DatasetDAOTest.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Arne Binder (arne.b.binder@gmail.com) on 13.10.2015.
  */
@@ -30,13 +32,31 @@ public class DatasetDAOTest {
 
     @Test
     public void test() throws Exception {
+
+        long countBefore = datasetDAO.count();
+
         logger.info("create user and save it");
         User user = new User("hallo", "welt", User.roleUser);
         userDAO.save(user);
         logger.info("create dataset");
         Dataset dataset = new Dataset(user, OwnedResource.Visibility.PUBLIC, "name", "description");
 
-        // does not work: the current session have to be authenticated
+        // does not work: the current session have to be authenticated for accessibility checks!
         // datasetDAO.save(dataset);
+        // use this instead:
+        datasetDAO.getRepository().save(dataset);
+        long id = dataset.getId();
+        logger.info("id of saved dataset: "+id);
+
+        assertEquals(countBefore+1,datasetDAO.count());
+
+        logger.info("fetch and check saved dataset");
+        Dataset fetchedDataset = datasetDAO.getRepository().findOneById(id);
+        assertEquals("name", fetchedDataset.getName());
+        assertEquals("description", fetchedDataset.getDescription());
+        assertEquals(0, fetchedDataset.getTotalEntities());
+        datasetDAO.getRepository().delete(fetchedDataset);
+
+        assertEquals(countBefore,datasetDAO.count());
     }
 }
