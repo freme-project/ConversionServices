5104341f7c0dd0c2cbbe5f44a7cbc6fdccbf16de
jnehring <gmnoname@gmx.de>
2016-08-29 15:36:53 +0200

    #31 update unit test

--- src/test/java/eu/freme/common/conversion/rdf/JenaRDFConverterTest.java
+++ src/test/java/eu/freme/common/conversion/rdf/JenaRDFConverterTest.java
@@ -51,7 +51,7 @@ public class JenaRDFConverterTest {
 		Model model = ModelFactory.createDefaultModel();
 		converter.plaintextToRDF(model, plaintext, language, prefix);
 
-		assertTrue(countStatements(model.listStatements()) == 5);
+		assertTrue(countStatements(model.listStatements()) == 6);
 
 		Property isString = model.createProperty(RDFConstants.nifPrefix
 				+ "isString");
